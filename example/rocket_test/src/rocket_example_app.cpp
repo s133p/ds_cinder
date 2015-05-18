b0b4df8ab1242c4935955bbc3e57c281931413ad
Sepehr Laal <sepehr.laal@gmail.com>
2015-05-17 23:35:25 -0700

    ROCKET: finished the example, good for a start on this.

--- example/rocket_test/src/rocket_example_app.cpp
+++ example/rocket_test/src/rocket_example_app.cpp
@@ -13,6 +13,7 @@ public:
 void RocketExampleApp::setupServer()
 {
     ds::ui::Sprite &rootSprite = mEngine.getRootSprite();
+
     auto rocket_sprite = rootSprite.addChildPtr(new ds::ui::Rocket(mEngine));
     rocket_sprite->setCenter(0.5f, 0.5f);
     rocket_sprite->setSize(300.0f, 225.0f);
@@ -20,6 +21,14 @@ void RocketExampleApp::setupServer()
     rocket_sprite->loadHtml("%APP%/assets/demo.rml");
     rocket_sprite->enable(true);
     rocket_sprite->enableMultiTouch(ds::ui::MULTITOUCH_CAN_POSITION | ds::ui::MULTITOUCH_CAN_SCALE);
+    
+    /*rocket_sprite = rootSprite.addChildPtr(new ds::ui::Rocket(mEngine));
+    rocket_sprite->setCenter(1.0f, 0);
+    rocket_sprite->setSize(1024.0f, 768.0f);
+    rocket_sprite->setPosition(mEngine.getWorldWidth(), 0);
+    rocket_sprite->loadHtml("%APP%/data/sepehr.html");
+    rocket_sprite->enable(true);
+    rocket_sprite->enableMultiTouch(ds::ui::MULTITOUCH_CAN_POSITION | ds::ui::MULTITOUCH_CAN_SCALE);*/
 }
 
 // This line tells Cinder to actually create the application
